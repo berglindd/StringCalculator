5cb2203153423b5824998ca613e104bd28f8bdfc
berglind <berglinds14@ru.is>
2017-10-15 20:24:12 +0000

    Structure

---
+++ bin/compile_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+javac -classpath "Classes/;src/test/resources/junit-4.10.jar" src/test/java/is/ru/stringcalculator/CalculatorTest.java -d Classes
---
+++ bin/unit_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp "Classes/;src/test/resources/junit-4.10.jar;src/test/resources/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore is.ru.stringcalculator.CalculatorTest
